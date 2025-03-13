3b1d39cbc6b338ea1fb2e076989a27ccf5c92d59
JANC_PC\czech <czechumennakompie@gmail.com>
2025-03-13 10:54:44 +0100

    Added cVRP problem.

---
+++ src/KnapsackProblem/KnapsackProblem/KnapsackProblem/Problems/CapacitatedVehicleRoutingProblem.cs
@@ -0,0 +1,12 @@
+﻿namespace ProblemSolvers.Problems
+{
+    public class CapacitatedVehicleRoutingProblem
+    {
+        // Problem: https://en.wikipedia.org/wiki/Vehicle_routing_problem
+
+        public const int TRUCK_CAPACITY = 20;
+
+        // each index is a number of a city, the value is a capacity taken from the truck
+        private readonly int[] CityAmountTakers = [10, 5, 12, 11];
+    }
+}
