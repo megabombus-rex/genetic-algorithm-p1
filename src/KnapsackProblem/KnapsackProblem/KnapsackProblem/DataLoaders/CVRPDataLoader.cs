f248c45a8cbc340d51d88fd06c5eee7e93ff7b07
megabombus-rex <czechumennakompie@gmail.com>
2025-03-20 15:56:43 +0100

    Added base of data loader for CVRP

---
+++ src/KnapsackProblem/KnapsackProblem/KnapsackProblem/DataLoaders/CVRPDataLoader.cs
@@ -0,0 +1,32 @@
+﻿using ProblemSolvers.Problems;
+
+namespace ProblemSolvers.DataLoaders
+{
+    public class CVRPDataLoader : IDataLoader<CVRProblem>
+    {
+        public CVRPDataLoader()
+        {
+
+        }
+
+
+        // data needed -> truck capacity, cities list
+        // JSON data is ok, maybe implement different loaders for different inputType
+        CVRProblem IDataLoader<CVRProblem>.LoadData(string filePath)
+        {
+            if (string.IsNullOrEmpty(filePath))
+            {
+                throw new ArgumentNullException($"Filepath is null. Problem {typeof(CVRProblem)} could not be loaded.");
+            }
+
+            if (!File.Exists(filePath))
+            {
+                throw new FileNotFoundException($"File {filePath} does not exist.");
+            }
+
+            var cvrProblem = new CVRProblem();
+
+            return new CVRProblem();
+        }
+    }
+}
---
+++ src/KnapsackProblem/KnapsackProblem/KnapsackProblem/DataLoaders/IDataLoader.cs
@@ -0,0 +1,7 @@
+﻿namespace ProblemSolvers.DataLoaders
+{
+    public interface IDataLoader<T>
+    {
+        T LoadData(string filePath);
+    }
+}
