144d6d36cc2dec1bc0c73880f3807df5851a4b8d
megabombus-rex <czechumennakompie@gmail.com>
2025-03-23 18:04:38 +0100

    Moved program to a defined scope.

--- src/KnapsackProblem/KnapsackProblem/KnapsackProblem/Program.cs
+++ src/KnapsackProblem/KnapsackProblem/KnapsackProblem/Program.cs
@@ -3,15 +3,10 @@ using ProblemSolvers.CommonTypes.GenericData;
 using ProblemSolvers.DataLoaders;
 using ProblemSolvers.DataLoaders.CVRP;
 using ProblemSolvers.Problems;
-using ProblemSolvers.Solvers.Genetic;
-using ProblemSolvers.Solvers.Genetic.Crossoverers.BinaryCrossoverers;
 using ProblemSolvers.Solvers.Genetic.Crossoverers.CombinatoralCrossoverers;
-using ProblemSolvers.Solvers.Genetic.Mutators.BinaryMutators;
 using ProblemSolvers.Solvers.Genetic.Mutators.CombinatoralMutators;
 using ProblemSolvers.Solvers.Genetic.Selectors;
-using ProblemSolvers.Solvers.Greedy;
-using ProblemSolvers.Solvers.RandomSearch;
-using ProblemSolvers.Solvers.SimulatedAnnealing;
+using ProblemSolvers.TestData.ProblemRunners;
 
 public class Program
 {
@@ -21,7 +16,6 @@ public class Program
         // create a problem with methods for Evaluation and for encoded data translation to problem's data
         // setup a solver for given problem
         var testDataPath = string.Format("{0}\\{1}", Environment.CurrentDirectory, "..\\..\\..\\TestData");
-
 
         // setup differently
         // RouletteSelector - const, no stuff to change
@@ -34,30 +28,25 @@ public class Program
 
         //var jsonDataLoader = new CVRPJsonDataLoader();
         //var cvrpOne = jsonDataLoader.LoadData(testDataPath + "\\CVRProblem_T2.json");
+        var sourceFile = testDataPath + "\\VRP\\Easy\\A-n32-k5.vrp";
         dataLoader = new CVRPvrpDataLoader();
-        var cvrpOne = dataLoader.LoadData(testDataPath + "\\VRP\\Easy\\A-n32-k5.vrp");
+        var cvrpOne = dataLoader.LoadData(sourceFile);
 
         // setup algorithm generic data
         var GAdataCVRP = new GeneticAlgorithmGenericData(GenerationsAmount: 100, PopulationSize: 100, CrossoverProbability: 0.7, MutationProbability: 0.1);
-        
         // the same amount of Generations as for each genome in genetic algorithm per generation
         var RSdataCVRP = new RandomSearchGenericData(GenerationsAmount: GAdataCVRP.GenerationsAmount * GAdataCVRP.PopulationSize);
-        
         var SAdataCVRP = new SimulatedAnnealingGenericData(100, 1.0, 0.0001, 0.9);
 
         // setup the solver
         var crossovererCVRP = new OrderedCrossoverer();
         var mutatorCVRP = new InvertedCombinationMutator();
 
+        var runner = new CVRProblemRunner(cvrpOne, GAdataCVRP, RSdataCVRP, SAdataCVRP, SelectionType.Tournament, crossovererCVRP, mutatorCVRP, 5);
 
-        var cvrpGeneticSolver = new CVRPGeneticSolver(SelectionType.Roulette, crossovererCVRP, mutatorCVRP, GAdataCVRP, cvrpOne);
-        var cvrpGreedySolver = new CVRPGreedySolver(cvrpOne);
-        var cvrpRandomSolver = new CVRPRandomSearchSolver(cvrpOne, RSdataCVRP);
-        var cvrpAnnealingSolver = new CVRPSimulatedAnnealingSolver(cvrpOne, SAdataCVRP);
-
-        var restultCVRPGenetic = cvrpGeneticSolver.FindOptimalSolution();
-        var restultCVRPGreed = cvrpGreedySolver.FindOptimalSolution();
-        var restultCVRPRandom = cvrpRandomSolver.FindOptimalSolution();
-        var restultCVRPAnnealing = cvrpAnnealingSolver.FindOptimalSolution();
+        runner.RunProblem();
+        
+        //var dataSaver = new CVRPDataSaverGeneticAlgorithm(resultCVRPGenetic, cvrpGeneticSolver, GAdataCVRP, sourceFile);
+        //dataSaver.AppendFileFromData();
     }
 }
---
+++ src/KnapsackProblem/KnapsackProblem/KnapsackProblem/TestData/ProblemRunners/CVRProblemRunner.cs
@@ -0,0 +1,109 @@
+﻿using ProblemSolvers.CommonTypes.BestData;
+using ProblemSolvers.CommonTypes.GAEnums;
+using ProblemSolvers.CommonTypes.GenericData;
+using ProblemSolvers.Problems;
+using ProblemSolvers.Solvers.Genetic;
+using ProblemSolvers.Solvers.Genetic.Crossoverers.CombinatoralCrossoverers;
+using ProblemSolvers.Solvers.Genetic.Mutators.CombinatoralMutators;
+using ProblemSolvers.Solvers.Genetic.Selectors;
+using ProblemSolvers.Solvers.Greedy;
+using ProblemSolvers.Solvers.RandomSearch;
+using ProblemSolvers.Solvers.SimulatedAnnealing;
+
+namespace ProblemSolvers.TestData.ProblemRunners
+{
+    public class CVRProblemRunner
+    {
+        private CVRProblem _problem;
+
+        private CVRPGeneticSolver _geneticSolver;
+        private CVRPGreedySolver _greedySolver;
+        private CVRPRandomSearchSolver _randomSearchSolver;
+        private CVRPSimulatedAnnealingSolver _annealingSolver;
+
+        public CVRProblemRunner(CVRProblem problem, 
+            GeneticAlgorithmGenericData GAData, RandomSearchGenericData RSData, SimulatedAnnealingGenericData SAData,
+            SelectionType selectionType, CombinatoralCrossoverer crossoverer, CombinatoralMutator mutator, int tournamentContestantsAmount)
+        {
+            _problem = problem;
+
+            _geneticSolver = new CVRPGeneticSolver(selectionType, crossoverer, mutator, GAData, problem);
+            _greedySolver = new CVRPGreedySolver(problem);
+            _randomSearchSolver = new CVRPRandomSearchSolver(problem, RSData);
+            _annealingSolver = new CVRPSimulatedAnnealingSolver(problem, SAData);
+
+            TournamentSelector.TournamentContestants = tournamentContestantsAmount;
+        }
+
+        public void RunProblem()
+        {
+            var resultCVRPGreed = _greedySolver.FindOptimalSolution();
+
+            var geneticSolutions = new List<BestCVRPData>();
+            var randomSolutions = new List<BestCVRPData>();
+            var annealingSolutions = new List<BestCVRPDataSimulatedAnnealing>();
+
+            int iterationCount = 10;
+
+            for (int i = 0; i < iterationCount; i++)
+            {
+                var resultCVRPGenetic = _geneticSolver.FindOptimalSolution();
+                var resultCVRPRandom = _randomSearchSolver.FindOptimalSolution();
+                var resultCVRPAnnealing = _annealingSolver.FindOptimalSolution();
+
+                geneticSolutions.Add(resultCVRPGenetic);
+                randomSolutions.Add(resultCVRPRandom);
+                annealingSolutions.Add(resultCVRPAnnealing as BestCVRPDataSimulatedAnnealing);
+            }
+
+            var sumOfFitnessesGen = 0.0;
+            var bestFitnessGen = double.MaxValue;
+
+            foreach (var genSol in geneticSolutions)
+            {
+                sumOfFitnessesGen += genSol.Fitness;
+                if (genSol.Fitness < bestFitnessGen)
+                {
+                    bestFitnessGen = genSol.Fitness;
+                }
+            }
+            var meanFitnessGen = sumOfFitnessesGen / (double)geneticSolutions.Count;
+
+            var sumOfFitnessesRan = 0.0;
+            var bestFitnessRan = double.MaxValue;
+
+            foreach (var ranSol in randomSolutions)
+            {
+                sumOfFitnessesRan += ranSol.Fitness;
+                if (ranSol.Fitness < bestFitnessRan)
+                {
+                    bestFitnessRan = ranSol.Fitness;
+                }
+            }
+            var meanFitnessRan = sumOfFitnessesRan / (double)randomSolutions.Count;
+
+            var sumOfFitnessesSA = 0.0;
+            var bestFitnessSA = double.MaxValue;
+
+            foreach (var saSol in annealingSolutions)
+            {
+                sumOfFitnessesSA += saSol.Fitness;
+                if (saSol.Fitness < bestFitnessSA)
+                {
+                    bestFitnessSA = saSol.Fitness;
+                }
+            }
+            var meanFitnessSA = sumOfFitnessesSA / (double)annealingSolutions.Count;
+
+            ShowResults(meanFitnessGen, bestFitnessGen, iterationCount, "Genetic Algorithm");
+            ShowResults(meanFitnessRan, bestFitnessRan, iterationCount, "Random Search Algorithm");
+            ShowResults(meanFitnessSA, bestFitnessSA, iterationCount, "Simulated Annealing Algorithm");
+            ShowResults(resultCVRPGreed.Fitness, resultCVRPGreed.Fitness, 1, "Greedy Algorithm");
+        }
+
+        private void ShowResults(double mean, double best, int iterationCount, string algorithmSelected)
+        {
+            Console.WriteLine(algorithmSelected + $":\nMean fitness: {mean}.\nBest fitness: {best}.\n Number of runs the algorithm had: {iterationCount}.");
+        }
+    }
+}
