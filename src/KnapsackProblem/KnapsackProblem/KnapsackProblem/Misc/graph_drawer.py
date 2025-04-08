6d8daa6a85f74d6213237b9e1bea86851a44c261
megabombus-rex <czechumennakompie@gmail.com>
2025-04-08 16:15:02 +0200

    Graphs update.

--- src/KnapsackProblem/KnapsackProblem/KnapsackProblem/Misc/graph_drawer.py
+++ src/KnapsackProblem/KnapsackProblem/KnapsackProblem/Misc/graph_drawer.py
@@ -34,16 +34,16 @@ import matplotlib.pyplot as plt
 #plt.plot(gen100_popsizes, gen100_bestValues, label = "Best")
 #plt.plot(gen100_popsizes, gen100_worstValues, label = "Worst")
 
-#SA_AlphaValues = [0.1, 0.25, 0.5, 0.9, 0.99]
+SA_AlphaValues = [0.1, 0.25, 0.5, 0.9, 0.99]
 #SA_i100_t01_meanValues_exp = [1186, 1150, 1090, 1007, 976]
 #SA_i100_t01_bestValues_exp = [1079, 971, 937, 870, 867]
 #SA_i100_t01_worstValues_exp = [1312, 1252, 1226, 1180, 1151]
-#SA_i100_t01_runtimes_exp = [4, 4, 6, 47, 203]
+SA_i100_t01_runtimes_exp = [4, 4, 6, 47, 203]
 
 #SA_i100_t01_meanValues_lin = [1131, 1157, 1342, 1342, 1299]
 #SA_i100_t01_bestValues_lin = [1021, 1086, 1205, 1249, 1100]
 #SA_i100_t01_worstValues_lin = [1262, 1243, 1439, 1483, 1440]
-#SA_i100_t01_runtimes_lin = [5, 3, 1.5, 2.5, 1]
+SA_i100_t01_runtimes_lin = [5, 3, 1.5, 2.5, 1]
 
 #plt.xlabel("Alpha value")
 #plt.ylabel("Fitness score")
@@ -80,30 +80,29 @@ import matplotlib.pyplot as plt
 #plt.plot(SA_AlphaValues, SA_i100_t01_runtimes_lin, label = "Linear T change")
 #plt.xlabel("Alpha value")
 #plt.ylabel("Runtime [ms]")
-#plt.title("100 Generations GA, CC - 99%, MC - 5%")
+#plt.title("Exponential vs Linear T change")
 #plt.legend()
 #plt.show()
 # SA runtimes, Exp/Lin - finish
 
 
 # best instances
-#instances = ["An32k6", "An37k6", "An39k5", "An45k6", "An48k7", "An54k7", "An60k9"]
-#best_GA = [802, 968, 839, 981, 1140, 1286, 1479]
-#best_RSA = [1321, 1515, 1429, 1994, 2033, 2346, 2760]
-#best_SAA = [864, 1006, 934, 1093, 1197, 1350, 1591]
-#best_GrA = [1139, 1372, 1098, 1485, 1405, 1439, 2042]
-#optimal_v = [784, 949, 822, 944, 1073, 1167, 1354]
-
-#plt.plot(instances, best_GA, label = "Genetic Algorithm")
-#plt.plot(instances, best_RSA, label = "Random Search Algorithm")
-#plt.plot(instances, best_SAA, label = "Simulated Annealing Algorithm")
-#plt.plot(instances, best_GrA, label = "Greedy Algorithm")
-#plt.plot(instances, best_GrA, label = "Optimal")
+instances = ["An32k5", "An37k6", "An39k5", "An45k6", "An48k7", "An54k7", "An60k9"]
+best_GA = [802, 968, 839, 981, 1140, 1286, 1479]
+best_RSA = [1321, 1515, 1429, 1994, 2033, 2346, 2760]
+best_SAA = [864, 1006, 934, 1093, 1197, 1350, 1591]
+best_GrA = [1139, 1372, 1098, 1485, 1405, 1439, 2042]
+optimal_v = [784, 949, 822, 944, 1073, 1167, 1354]
+plt.plot(instances, best_GA, label = "Genetic Algorithm")
+plt.plot(instances, best_RSA, label = "Random Search Algorithm")
+plt.plot(instances, best_SAA, label = "Simulated Annealing Algorithm")
+plt.plot(instances, best_GrA, label = "Greedy Algorithm")
+plt.plot(instances, optimal_v, label = "Optimal")
 #
-#plt.xlabel("Instances")
-#plt.ylabel("Fitness score")
-#plt.title("Best fitness per instance")
+plt.xlabel("Instances")
+plt.ylabel("Fitness score")
+plt.title("Best fitness per instance")
 #
-#plt.legend()
-#plt.show()
+plt.legend()
+plt.show()
 # best instances - finish
